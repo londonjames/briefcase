6f53934bebe990831aa0e04c8bb899d2e71e08e0
londonjames <raybould@gmail.com>
2026-02-06 18:16:44 -0800

    Fix: remove brotli from Accept-Encoding (requests cant decompress it)

--- backend/scraper.py
+++ backend/scraper.py
@@ -12,7 +12,7 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
